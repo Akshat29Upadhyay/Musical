17f6fa0a20e1749f166d98a44c6a6b99efa878b3
Akshat Upadhyay <upadhyay.akshat29@gmail.com>
2024-07-28 16:38:39 +0530

    changes: added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import MusicSchoolTestimonials from "@/components/MusicSchoolTestimonials"
 import UpcomingWebinars from "@/components/UpcomingWebinars"
 import Image from "next/image";
 import Instructors from "@/components/Instructors";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <MusicSchoolTestimonials/>
       <UpcomingWebinars/>
       <Instructors/>
+      <Footer/>
     </main>
     
   );
